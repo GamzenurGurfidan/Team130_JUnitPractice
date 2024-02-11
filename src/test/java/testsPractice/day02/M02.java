583ccd9d3258ef09c66260495faffeeee4c245ac
GamzenurGurfidan <gamzenurguclu@gmail.com>
2024-02-11 17:30:36 +0300

    day01

--- src/test/java/testsPractice/day02/M02.java
+++ src/test/java/testsPractice/day02/M02.java
@@ -1,6 +1,10 @@
 package testsPractice.day02;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import utilities.ReusableMethods;
 import utilities.TestBase;
 
 public class M02 extends TestBase {
@@ -13,6 +17,20 @@ public class M02 extends TestBase {
     @Test
     public void test01(){
         driver.get("https://www.amazon.com/customer-preferences/country?ref_=icp_lop_mop_chg_ais&preferencesReturnUrl=/");
+
+        WebElement countryDDM = driver.findElement(By.id("icp-dropdown"));
+        Select select = new Select(countryDDM);
+
+        select.selectByIndex(19);
+        System.out.println(select.getFirstSelectedOption().getText());
+
+        ReusableMethods.bekle(2);
+
+        driver.findElement(By.className("a-button-input")).click();
+
+        ReusableMethods.bekle(2);
+
+        driver.quit();
 
     }
 
